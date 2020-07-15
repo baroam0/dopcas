d060367f60d5dba836510911f1afd24eb84fc41c
Abel Barrios <barriosabel0@hotmail.com>
2020-07-15 06:55:36 -0300

    Se agrega modelo de obras, inspectores y contratistas

---
+++ apps/inspectores/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class InspectoresConfig(AppConfig):
+    name = 'inspectores'
---
+++ apps/inspectores/models.py
@@ -0,0 +1,17 @@
+
+from django.db import models
+
+
+class Inspector(models.Model):
+    apellido = models.CharField(max_length=100, null=False, blank=False)
+    nombre = models.CharField(max_length=100, null=False, blank=False)
+    telelfono = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.apellido + ", " + self.nombre
+    
+    class Meta:
+        verbose_name_plural = "Inspectores"
+
+
+# Create your models here.

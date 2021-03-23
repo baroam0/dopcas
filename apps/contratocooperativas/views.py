e59c15249cd18d22b9b2c2ffd1cacdce11e17e1c
baroam0 <barrriosabel0@hotmail.com>
2021-03-23 19:18:44 -0300

    Se agrega separador de miles

--- apps/contratocooperativas/views.py
+++ apps/contratocooperativas/views.py
@@ -17,10 +17,10 @@ def detallecontratocooperativa(request, pk):
     resultado = ContratoCooperativa.objects.get(pk=pk)
     plazo = totalmes(resultado.fecha_inicio, resultado.fecha_fin)
     montocontrato = plazo * resultado.montomensual
-    montocontrato = round(float(montocontrato),2)
+    montocontrato = round(montocontrato,2)
     f_montocontrato = f'{montocontrato:n}'
 
-    montomensual = round(float(resultado.montomensual),2)
+    montomensual = round(resultado.montomensual,2)
     f_montomensual = f'{montomensual:n}'
 
     letramontomensual = numtxt(resultado.montomensual)
